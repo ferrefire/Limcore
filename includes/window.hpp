e0698dd6a3368c8c53b879cfc63dec63eb11284d
Ferre <ferremolenbeek@gmail.com>
2026-09-18 18:58:41 +0200

    Changed the default window presentmode.

--- includes/window.hpp
+++ includes/window.hpp
@@ -23,7 +23,8 @@ struct WindowConfig
 	bool fullscreen = true;
 	VkSurfaceCapabilitiesKHR capabilities{};
 	VkSurfaceFormatKHR format{};
-	VkPresentModeKHR presentMode = VK_PRESENT_MODE_FIFO_KHR;
+	//VkPresentModeKHR presentMode = VK_PRESENT_MODE_FIFO_KHR;
+	VkPresentModeKHR presentMode = VK_PRESENT_MODE_MAILBOX_KHR;
 	VkExtent2D extent{};
 };
 
